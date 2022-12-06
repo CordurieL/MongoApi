e652b9c0d76e3639d1c36f987c289c013f4031cd
CordurieL <lucas.cordurie9@etu.univ-lorraine.fr>
2022-12-06 17:46:07 +0100

    colors

--- public/index.php
+++ public/index.php
@@ -18,16 +18,35 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 const collection = <?php echo json_encode($collection->find()->toArray()); ?>;
+
+let redMarker = L.icon({
+  iconUrl: "https://github.com/pointhi/leaflet-color-markers/blob/master/img/marker-icon-red.png?raw=true",
+  iconSize: [25, 41]
+});
+let blueMarker = L.icon({
+  iconUrl: "https://github.com/pointhi/leaflet-color-markers/blob/master/img/marker-icon-blue.png?raw=true",
+  iconSize: [25, 41]
+});
+let orangeMarker = L.icon({
+  iconUrl: "https://github.com/pointhi/leaflet-color-markers/blob/master/img/marker-icon-orange.png?raw=true",
+  iconSize: [25, 41]
+});
+
 collection.forEach(function (doc) {
-  console.log(doc);
-  console.log(doc.geometry.y + " " + doc.geometry.x);
   let places = doc.places != null?doc.places:"?";
   let capacity = doc.capacity != null?doc.capacity:"?";
-  L.marker([doc.geometry.y, doc.geometry.x])
+  let iconDisplayed = blueMarker;
+  if (places == "?" || capacity == "?") {
+    iconDisplayed = orangeMarker;
+  } else if (places == 0) {
+    iconDisplayed = redMarker;
+  }
+    L.marker([doc.geometry.y, doc.geometry.x], {icon: iconDisplayed})
     .addTo(map)
     .bindPopup(
       doc.name + "<br>" + doc.address + "<br>" + places + "/" + capacity + " places disponibles"
-    );
+    )
 });
+
 
 </script>
